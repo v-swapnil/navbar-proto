1268462967411789dd0a382ac3ad9b0b7faa6497
swapnil <swapnil.suryajoshi@neosofttech.com>
2019-09-24 11:41:43 +0530

    Updates

---
+++ precache-manifest.0b2c3f6fbeaf592ca918ea60b218bdfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a5a2498c1e653973e401fc71d094de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61c5ea303681df2e5f86",
+    "url": "/static/css/main.0d0d2244.chunk.css"
+  },
+  {
+    "revision": "abf2ecac511de3590c1b",
+    "url": "/static/js/2.262bfd11.chunk.js"
+  },
+  {
+    "revision": "61c5ea303681df2e5f86",
+    "url": "/static/js/main.23743e7d.chunk.js"
+  },
+  {
+    "revision": "11569c7b8fde9529b138",
+    "url": "/static/js/runtime~main.cd9a5c0a.js"
+  }
+]);
